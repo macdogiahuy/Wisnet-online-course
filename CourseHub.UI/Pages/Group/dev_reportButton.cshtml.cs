8e338140fa4c1b0347c69e8195e411807496b844
Mạc Đỗ Gia Huy <macdogiahuy123@gmail.com>
2023-10-06 20:59:03 +0700

    add button report in group

---
+++ CourseHub.UI/Pages/Group/dev_reportButton.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CourseHub.UI.Pages.Group
+{
+    public class dev_reportButtonModel : PageModel
+    {
+
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
