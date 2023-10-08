9151c1749398e694dbc35384d23eabe9c4be2581
Mạc Đỗ Gia Huy <macdogiahuy123@gmail.com>
2023-10-09 02:11:45 +0700

    Update UI admin block user accout Done

--- CourseHub.UI/Areas/Admin/Helpers/AdminGlobal.cs
+++ CourseHub.UI/Areas/Admin/Helpers/AdminGlobal.cs
@@ -7,6 +7,7 @@ public class AdminGlobal
     public const string PAGE_INDEX = "/Index";
     public const string PAGE_USERS_INDEX = "/Users/Index";
     public const string PAGE_NOTIFICATIONS_INDEX = "/Notifications/Index";
+    public const string PAGE_BLOCK_USERS_ACCOUNT = "/BlockUsersAccount/Index";
 
     public const string LAYOUT_BASE = "_BaseLayout";
 }
